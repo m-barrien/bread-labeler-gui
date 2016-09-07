c2c78c0cb669d348c8d492cd35f9f8128001d847
m-barrien <mb33135@gmail.com>
2016-09-07 16:57:41 -0400

    Load images from input to class

--- label.py
+++ label.py
@@ -1,9 +1,13 @@
 #Python 3
 import tkinter as tk
+from PIL import Image, ImageTk
+from os import listdir
+from os.path import isfile, join
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+
         self.pack()
         self.create_widgets()
 
@@ -19,7 +23,18 @@ class Application(tk.Frame):
 
     def say_hi(self):
         print("hi there, everyone!")
-
+        
+class ImageDataset(object):
+	"""docstring for ImageDataset"""
+	"""Images path"""
+	in_path="./output/"
+	out_path="./labeling/"
+	def __init__(self):
+		super(ImageDataset, self).__init__()
+		self.files=[f for f in listdir(self.in_path) if isfile(join(self.in_path, f))]
+		print ("%d Images found " % (len(self.files)))
+			
+imgManager=ImageDataset()
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
